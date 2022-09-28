33b269f9048d0f0a2cb1cf812a26d09024ad5307
Monahlissa <salissa4@gmail.com>
2022-09-28 15:57:11 -0500

    added js file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,75 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return ``
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== ){
+
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== ){
+
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}  
+  
+  ## Table of Content
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Tests](#tests)
+  - [Contributing](#contributing)
+  - [Questions](#questions)
+  - [License](#license)
+
+  ## Installation
+
+  Install necessary dependencies by entering the below in command line:
+
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Tests
+
+  To run a test enter below in command line:
+
+  \`\`\`
+  ${data.test}
+  \`\`\`
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Questions
+
+  If you have any questions regarding repo, or issues please reach me at ${data.email}. Find more of my work at [${data.github}](https://github.com/${
+    data.github
+  }/).
+
+  ## License
+
+  ${renderLicenseSection(data.license)}
 `;
 }
 
