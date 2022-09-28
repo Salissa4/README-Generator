ce7267c2de7c17b9d95975001c22a6cf3faf8479
Monahlissa <salissa4@gmail.com>
2022-09-28 16:38:30 -0500

    update license section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,29 +2,35 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== 'None') {
-    return ``
+    return `![Github license](https://img.shields.io/badge/License-${license}-blue.svg)`;
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license !== ){
-
+  if (license !== 'None') {
+    return `\n* [License](#license)\n`;
   }
+  return '';
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license !== ){
+  if (license !== 'None') {
+    return `## License
 
+Project is licensed under ${license} license.`;
   }
+  return '';
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
 
   ## Description
   ${data.description}  
@@ -68,7 +74,7 @@ function generateMarkdown(data) {
   }/).
 
   ## License
-
+  ${renderLicenseLink(data.license)}
   ${renderLicenseSection(data.license)}
 `;
 }
